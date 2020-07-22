6b330754fa93a1bbaacc3c82efaa2d17001938b1
muhamedsuhail <muhamed.suhail1@gmail.com>
2020-07-22 10:46:53 +0530

    Updated settings

--- project4/settings.py
+++ project4/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
